03931289b92e40a3f886e76b8e04dfcc95a06131
Beniam Lemma <benonforever@gmail.com>
2025-12-18 09:03:12 +0300

    prisma.ts datasource change

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,13 +1,13 @@
 import { PrismaClient } from "@prisma/client";
 
-const globalForPrisma = global as unknown as {
+const globalForPrisma = globalThis as unknown as {
   prisma: PrismaClient | undefined;
 };
 
 export const prisma =
   globalForPrisma.prisma ??
   new PrismaClient({
-    log: ["query", "error", "warn"],
+    datasourceUrl: process.env.DATABASE_URL, // REQUIRED for Prisma 7
   });
 
 if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
